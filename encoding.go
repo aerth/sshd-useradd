7797b6aa666be1ec428516c7eb07d7864806a472
aerth <aerth@riseup.net>
2017-04-25 19:33:05 -0700

    decode json

---
+++ encoding.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+)
+
+type ServerResponse struct {
+	Message string `json:"message"`
+}
+
+func decode(b []byte) string {
+	var m ServerResponse
+	err := json.Unmarshal(b, m)
+	if err != nil {
+		println(err.Error())
+		return "{}"
+	}
+	return m.Message
+}
