9764004f76884be76c7ed9dcf6f20c771e907ebe
aerth <aerth@riseup.net>
2017-04-29 14:08:18 -0700

    dummy api server

---
+++ debughttp/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func init() {
+	log.SetPrefix("")
+	log.SetFlags(log.Ltime)
+}
+
+var addr = "127.0.0.1:8666"
+var handler = func(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseForm()
+		log.Println(r.Form)
+	}
+	log.Println(r.Method, r.UserAgent(), r.URL.Path)
+}
+
+func main() {
+	http.ListenAndServe(addr, http.HandlerFunc(handler))
+}
